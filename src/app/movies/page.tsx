c127f5adca49719c18252038cd19de7322fb1bc5
FeliciaBo <theskylla@gmail.com>
2026-05-13 14:26:34 +0200

    added /movies

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,13 @@
+export default function MoviesPage() {
+  return (
+    <div>
+      <h1>Movies Page</h1>
+
+      <ul>
+        <li>Interstellar</li>
+        <li>Inception</li>
+        <li>Dune</li>
+      </ul>
+    </div>
+  );
+}
