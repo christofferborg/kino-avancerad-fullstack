714e35322dfb42fe0dcc05b3314e448f2c89d779
marieapellLernia <marie.apell@lerniastudent.se>
2026-05-18 11:31:19 +0200

    added mobile view and change some in desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,13 +26,13 @@ export default function Home() {
         </h2>
       </section>
 
-      <section className="mx-auto grid max-w-8xl gap-3 px-4 py-2 md:grid-cols-2">
+      <section className="grid gap-3 md:mx-auto md:max-w-7xl md:px-4 py-2 md:grid-cols-2">
         <Image
           src="/img/kidds.png"
           alt="Kids Movie Time"
           width={700}
           height={400}
-          className="object-cover"
+          className="h-auto w-full object-cover"
         />
 
         <Image
@@ -40,11 +40,11 @@ export default function Home() {
           alt="Champagne Movie Night"
           width={700}
           height={400}
-          className="object-cover"
+          className="h-auto w-full object-cover"
         />
       </section>
 
-      <section className="mx-auto grid max-w-7xl gap-3 px-4 py-2 md:grid-cols-2">
+      <section className="grid gap-3 md:mx-auto md:max-w-7xl md:px-4 py-2 md:grid-cols-2">
         <Image
           src="/img/action.png"
           alt="Action Night"
@@ -53,59 +53,83 @@ export default function Home() {
           className="h-full w-full object-cover"
         />
 
-        <div className="bg-card px-5 py-2">
-          <h3 className="mb-1 text-xl font-bold">
+        <div className="bg-card px-10 py-8 md:px-5 md:py-2">
+          <h3 className="mb-4 text-center text-xl font-bold md:mb-1 md:text-left">
             Prislista
           </h3>
 
           <div className="space-y-1">
-            <div className="flex justify-between text-sm">
+            <div className="mx-auto flex max-w-[220px] justify-between text-sm md:max-w-none">
               <span>Vuxen</span>
               <span>200kr</span>
             </div>
 
-            <div className="flex justify-between text-sm">
+            <div className="mx-auto flex max-w-[220px] justify-between text-sm md:max-w-none">
               <span>Barn</span>
               <span>100kr</span>
             </div>
 
-            <div className="flex justify-between text-sm">
+            <div className="mx-auto flex max-w-[220px] justify-between text-sm md:max-w-none">
               <span>Student</span>
               <span>150kr</span>
             </div>
 
-            <div className="flex justify-between text-sm">
+            <div className="mx-auto flex max-w-[220px] justify-between text-sm md:max-w-none">
               <span>Pensionär</span>
               <span>150kr</span>
             </div>
           </div>
 
-          <p className="mt-2 text-sm text-muted text-sm">
+          <p className="mt-6 text-center text-sm text-muted md:mt-2 md:text-left">
             *Andra priser gäller under evenemang.
           </p>
         </div>
       </section>
 
-      <section className="mx-auto max-w-7xl px-4 py-12">
-        <div className=" bg-card p-8">
+      <section className="mx-auto max-w-7xl px-4 py-12 ">
+        <div className=" bg-card p-8 rounded-sm">
           <h2 className="mb-6 text-center text-2xl font-bold">
             På Kino
           </h2>
 
-          <div className="grid gap-8 md:grid-cols-3 justify-items-center">
-            <ul className="space-y-2 text-muted">
+          {/* MOBILE */}
+          <div className="md:hidden">
+            <p className="mb-6 text-center text-main">
+              Bubbel & chark
+            </p>
+
+            <div className="grid grid-cols-2 gap-y-3 text-main justify-items-center">
+              <div className="space-y-2">
+                <p>Restaurang</p>
+                <p>Bar</p>
+                <p>Events</p>
+                <p>Barnvagnsbio</p>
+              </div>
+
+              <div className="space-y-2">
+                <p>Klassiker</p>
+                <p>Barnbio</p>
+                <p>Kiosk</p>
+                <p>Pensionärsbio</p>
+              </div>
+            </div>
+          </div>
+
+          {/* DESKTOP */}
+          <div className="hidden justify-items-center gap-8 md:grid md:grid-cols-3">
+            <ul className="space-y-2 text-main">
               <li>Restaurang</li>
               <li>Bar</li>
               <li>Events</li>
             </ul>
 
-            <ul className="space-y-2 text-muted">
+            <ul className="space-y-2 text-main">
               <li>Barnvagnsbio</li>
               <li>Pensionärsbio</li>
               <li>Bubbel och chark</li>
             </ul>
 
-            <ul className="space-y-2 text-muted">
+            <ul className="space-y-2 text-main">
               <li>Klassiker</li>
               <li>Barnbio</li>
               <li>Kiosk</li>
