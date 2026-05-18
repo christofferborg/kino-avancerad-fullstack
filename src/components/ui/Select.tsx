80f6c76f02389c142011ae808a426e39cf968d5a
FeliciaBo <theskylla@gmail.com>
2026-05-18 14:44:48 +0200

    px to tailwind

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -19,7 +19,7 @@ export default function Select({
         font-semibold
         text-base
         rounded-xl
-        min-w-[192px]
+        min-w-48
         h-auto
         appearance-none
         border
