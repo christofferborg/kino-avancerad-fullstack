776f17d5db59516c460dac2331bdae8bb8284253
Kevinekmen <kevin.ekmen@lerniastudent.se>
2026-05-26 14:01:31 +0200

    Skapat filmsidan med de verktyg som vi bygger projektet i

---
+++ src/app/movies/[id]/page.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+
+interface MovieObject {
+  id: string;
+  title: string;
+  plot: string;
+  rating: string;
+  genre: string;
+  img: string;
+}
+
+async function getMovies() {
+  const res = await fetch("http://localhost:3000/api/movies", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch movies");
+  }
+
+  return res.json();
+}
+
+export default async function MovieInfoPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+
+  const { id } = await params;
+
+  const movies: MovieObject[] = await getMovies();
+
+  console.log(params.id);
+  console.log(movies);
+
+  const movie = movies.find(
+    (movie) => movie.id === id
+  );
+
+  if (!movie) {
+    return <div>Filmen kunde inte hittas.</div>;
+  }
+
+  return (
+    <section className="flex justify-center px-4 mb-[200px]">
+
+      <div className="flex flex-col lg:flex-row gap-4 items-center mx-auto">
+
+        {/* Poster */}
+        <Image
+          src={movie.img}
+          alt={movie.title}
+          width={400}
+          height={600}
+          className="w-[400px] h-[600px] object-cover rounded-[10px]"
+        />
+
+        {/* Right Side */}
+        <div className="flex flex-col items-center">
+
+          {/* Info Box */}
+          <div className="bg-[#0E131B] p-8 w-[400px] rounded-[10px] lg:h-[426px]">
+
+            <p className="text-gray-400 mb-4">
+              ★ {movie.rating}
+            </p>
+
+            <h1 className="text-4xl font-bold mb-4">
+              {movie.title}
+            </h1>
+
+            <p className="text-gray-400 mb-6">
+              {movie.genre}
+            </p>
+
+            <p className="text-white leading-8">
+              {movie.plot}
+            </p>
+
+          </div>
+
+          {/* Review Buttons */}
+          <button className="flex items-center justify-between bg-[#0E131B] text-white text-left text-base h-[72px] w-[400px] mt-4 px-8 rounded-[10px] hover:text-[#179A90] transition">
+
+            <span>Recensioner</span>
+
+            <span>›</span>
+
+          </button>
+
+          <button className="flex items-center justify-between bg-[#0E131B] text-white text-left text-base h-[72px] w-[400px] mt-4 px-8 rounded-[10px] hover:text-[#179A90] transition">
+
+            <span>Skriv recension</span>
+
+            <span>›</span>
+
+          </button>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
