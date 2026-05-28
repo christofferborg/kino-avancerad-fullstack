bbd06fc92aa4e97d89975f515730be29df524743
FeliciaBo <theskylla@gmail.com>
2026-05-28 16:17:55 +0200

    added more tests to E2E

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   },
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -1,18 +1,24 @@
-//kolla att det går att navigera till movies sidan 
-// och statsikt innehåll finns
-
 describe('Movies page', () => {
 
-  it('Navigate to movies page', () => {
-
-    cy.visit('http://localhost:3000')
-
+  it('navigates to movies page with navigation bar, from home', () => {
+    cy.visit('/')
     cy.get('[data-cy="movies-link"]').click()
-
     cy.location('pathname').should('eq', '/movies')
+  })
 
-    cy.get('button').should('contain', 'Senaste filmerna')
+  describe('on the movies page', () => {
+    beforeEach(() => {
+      cy.visit('/movies')
+    })
 
+    it('shows initial 10 movies on load', () => {
+      cy.get('[data-cy="movie-img"]').should('have.length', 10)
+    })
+
+    it('shows static UI elements', () => {
+      cy.get('button').should('contain', 'Senaste filmerna')
+      cy.contains('Visa mer filmer').should('exist')
+    })
   })
 
 })
