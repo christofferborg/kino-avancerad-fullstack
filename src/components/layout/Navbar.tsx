871d2dda46156844197cf402bb8163e4c4a3394e
FeliciaBo <theskylla@gmail.com>
2026-05-28 11:59:13 +0200

    removed path to login page

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -372,7 +372,7 @@ const [menuOpen, setMenuOpen] = useState(false);
 
       
       <Link
-        href="/login"
+        href="/#"
         className="w-full flex items-center justify-between px-4 py-3 mt-10 rounded-[5px] hover:bg-[#0E131B] active:bg-[#0E131B] transition"
        >
        <div className="flex items-center gap-4">
