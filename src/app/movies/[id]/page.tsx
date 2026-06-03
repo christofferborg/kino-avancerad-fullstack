41f5c90b124473dfe1de14903fe8d38cbf988633
Christoffer Borg <christofferborg@me.com>
2026-06-03 15:47:17 +0200

    Fixed broken Link

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -63,7 +63,7 @@ export default async function MovieInfoPage({
           {/* Review Buttons */}
           <button className="flex items-center justify-between bg-card text-main text-left text-base h-[72px] w-[400px] mt-4 px-8 rounded-[10px] hover:text-muted transition">
 
-            <span>Recensioner</span>
+            <Link href={`/movies/${id}/comments`}>Recensioner</Link>
 
             <span>›</span>
           </button>
